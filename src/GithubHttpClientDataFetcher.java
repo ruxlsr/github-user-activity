7495047961860079c757a4ceb320f38dd70b38fb
ruxlsr <russel.lontsi@facsciences-uy1.cm>
2024-10-21 12:03:26 +0100

    error handling

--- src/GithubHttpClientDataFetcher.java
+++ src/GithubHttpClientDataFetcher.java
@@ -14,17 +14,31 @@ public class GithubHttpClientDataFetcher implements GithubDataFetcher{
     @Override
     public JSONArray fetchAllEvent(String url) {
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(URI.create(url)).header("Content-Type", "application/json").GET().build();
+        HttpRequest request = HttpRequest.newBuilder(URI.create(url)).header("Accept", "application/vnd.github+json" ).GET().build();
         HttpResponse<String> response;
+
         try {
             response = client.send(request, BodyHandlers.ofString());
-            
-            if(response.statusCode() == 200 && response.body() != null){
-                client.close();
-                //System.err.println(response.body());
-                return new JSONArray(response.body());
-            }else 
-                System.err.println("Une erreur: "+response.statusCode());
+
+            switch (response.statusCode()) {
+                case 200 -> {
+                    System.out.println("Status : OK");
+                    return new JSONArray(response.body());
+                }
+                case 304 -> {
+                    System.out.println("Status: not modified");
+                }
+                case 404 -> {
+                    System.err.println("Error: not Found");
+                }
+                 case 403 -> {
+                    System.err.println("Error: forbiden");
+                }
+                default-> {
+                    System.err.println("not Handled Error: "+response.statusCode());
+                }
+                    
+            }
         } catch (IOException e) {
             System.err.println("Error:"+e.getMessage());
         } catch (InterruptedException e) {
